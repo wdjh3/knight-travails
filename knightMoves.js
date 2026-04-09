c9a6e32e3a655e3a91bcd76934087ca5393e1de9
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2026-04-09 12:46:37 +0800

    feat: Add knightMovesList

---
+++ knightMoves.js
@@ -0,0 +1,16 @@
+export { knightMoves };
+
+const knightMovesList = [
+  [1, 2],
+  [2, 1],
+  [2, -1],
+  [1, -2],
+  [-1, -2],
+  [-2, -1],
+  [-2, 1],
+  [-1, 2],
+]
+
+function knightMoves([start, end]) {
+  const visitedSquares = [];
+}
