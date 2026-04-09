bb8e24966d47c825b7282c4c00f762ca3985de56
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2026-04-09 12:50:45 +0800

    feat: Add isValidSquare()

--- knightMoves.js
+++ knightMoves.js
@@ -9,8 +9,15 @@ const knightMovesList = [
   [-2, -1],
   [-2, 1],
   [-1, 2],
-]
+];
 
 function knightMoves([start, end]) {
   const visitedSquares = [];
+  if (!isValidSquare(start), !isValidSquare(end)) {
+    throw new Error("Invalid squares as input")
+  }
+}
+
+function isValidSquare([x, y]) {
+  return x >= 0 && x < 8 && y >= 0 && y < 8;
 }
